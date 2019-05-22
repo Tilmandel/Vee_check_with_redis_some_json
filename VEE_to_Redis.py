a713ac8ed7d08d44682ce5a462da3b5d2ef007b4
Tilmandel <39954992+Tilmandel@users.noreply.github.com>
2019-05-22 12:57:01 +0200

    Update VEE_to_Redis.py

--- VEE_to_Redis.py
+++ VEE_to_Redis.py
@@ -6,30 +6,19 @@ from back_end_Vee_Checker import _take_data_from_server,_write_to_server,_vee_ch
 server = redis.Redis('3.8.101.205', charset="utf-8", decode_responses=True, db=0)
 var_alarm_vee = 0.006
 var_alarm_btc = 8300
-var_date_of_day = []
 var_current_price_vee = []
 var_current_price_btc = []
-var_arch_VEE_price = []
-var_arch_BTC_price = []
 var_result_list = {}
-var_clear_data_list = [var_date_of_day,
-                       var_current_price_vee,
-                       var_current_price_btc,
-                       var_arch_VEE_price,
-                       var_arch_BTC_price]
+var_clear_data_list = [var_current_price_vee,
+                       var_current_price_btc]
 counter = 0
 var_DB_data,var_DB_data_sorted = _take_data_from_server(server)
-_data_to_lists()
+var_date_of_day,var_arch_VEE_price,var_arch_BTC_price =_data_to_lists(var_DB_data_sorted,var_DB_data)
 var_result_list['VEE'] = _vee_checker()
 var_result_list['VEE1'] = _vee_checker()
 var_result_list['BTC'] = _btc()
 var_result_list['BTC1'] = round(var_arch_BTC_price[-1])
 #=======================================================
-def _data_to_lists():
-    for i in var_DB_data_sorted:
-        var_date_of_day.append("{:>5s}".format(i))
-        var_arch_BTC_price.append("{:>10f}".format(var_DB_data[i]['BTC']))
-        var_arch_VEE_price.append("{:>10f}".format(var_DB_data[i]['VEE']))
 def _replace(string):
     delete = str(string)
     delete = delete.replace(",", " ")
@@ -83,7 +72,7 @@ while True:
             _clearing_current_session_data()
             _take_data_from_server(server)
             var_DB_data,var_DB_data_sorted = _take_data_from_server(server)
-            _data_to_lists()
+            var_date_of_day,var_arch_VEE_price,var_arch_BTC_price =_data_to_lists(var_DB_data_sorted,var_DB_data)
             counter = 1
         if time_H_M == '12:01'or time_H_M == '18:51' or time_H_M == '18:01' or time_H_M == '23:50' and counter == 1:
             counter = 0
